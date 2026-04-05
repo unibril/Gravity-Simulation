8ff55dbd16033f15899325c1c215ecb6fd6edcf1
Unibril Karki <unibrilkarki@gmail.com>
2026-04-05 15:37:59 +0545

    phase 3 done

--- 2objectgravity.py
+++ 2objectgravity.py
@@ -2,18 +2,26 @@ import math
 import time
 import os 
 
-def draw_grid(A,B,grid_size,initial_position):
+def draw_grid(A,B,grid_size,initial_position,trail):
     os.system('cls')
     grid = [['.'] * grid_size for _ in range(grid_size)]
-    grid[grid_size // 2][grid_size // 2] = A.symbol
     scale = grid_size / (initial_position * 2)
+    for tx, ty in trail:
+        tr_col = int(grid_size // 2 + tx * scale)
+        tr_row = int(grid_size // 2 - ty * scale)
+        if 0 <= tr_row < grid_size and 0 <= tr_col < grid_size:
+            grid[tr_row][tr_col] = '*'
+    grid[grid_size // 2][grid_size // 2] = A.symbol
+    
     grid_col = int(grid_size // 2 + B.x * scale)
-    grid_row = int(grid_size // 2 - B.y * scale) 
+    grid_row = int(grid_size // 2 - B.y * scale)
     if 0 <= grid_row < grid_size and 0 <= grid_col < grid_size:
         grid[grid_row][grid_col] = B.symbol
+    
     for row in grid:
         print(' '.join(row))
     
+  
     
 
 class Body:
@@ -45,6 +53,7 @@ A = Body(x=0, y=0, vx=0, vy=0, mass=mass_a, symbol='A')
 B = Body(x=initial_position, y=0, vx=initial_velocity_x_component, vy=initial_velocity_y_component, mass=masses, symbol='B')
 
 print("Please press ctl+c to stop the simulation. The simulation will automatically stop on a collision or escape.")
+trail = []
 while True:
     r = math.sqrt((B.x - A.x)**2 + (B.y - A.y)**2)
     if r < 5:  
@@ -71,9 +80,8 @@ while True:
     B.x += B.vx * dt
     B.y += B.vy * dt
 
-
-    print((B.x, B.y), r, math.hypot(B.vx, B.vy))
-    draw_grid(A, B, grid_size=40, initial_position=initial_position)
+    trail.append((B.x, B.y))
+    draw_grid(A, B, grid_size=40, initial_position=initial_position, trail=trail)
     time.sleep(dt)
     
 
